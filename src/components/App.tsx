cb3f9d3885b419476e072b561b6601c9a5df1c71
Sean O'Connor <seanoconnor941@gmail.com>
2021-02-08 21:06:59 -0700

    Minor text changes

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,7 +2,18 @@ import React from "react";
 import logo from "../assets/g4w@1x.svg";
 
 const App = () => {
-  return <img src={logo} alt="Gamers4War" aria-label="Gamers4War logo" />;
+  return (
+    <>
+      <img src={logo} alt="Gamers4War" aria-label="Gamers4War logo" />
+      <footer>
+        <p>
+          &copy; 2021 Gamers4War.com
+          <br />
+          <a href="https://github.com/seaneoo/g4w">Source code</a>
+        </p>
+      </footer>
+    </>
+  );
 };
 
 export default App;
