0e400d97ffd9605db96b0f3cb0b1c2f1712afe2e
Sean O'Connor <seanoconnor941@gmail.com>
2021-02-12 08:04:53 -0700

    Hero component

---
+++ src/components/hero/Hero.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <article className="hero-wrapper">
+      <div className="container"></div>
+    </article>
+  );
+};
+
+export default Hero;
